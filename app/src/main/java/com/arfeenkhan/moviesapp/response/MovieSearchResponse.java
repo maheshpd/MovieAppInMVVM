26976e213ca8f110a2aff82847bab3de1f9ee2b7
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-05 22:10:39 +0530

    Retrofit Response Object

---
+++ app/src/main/java/com/arfeenkhan/moviesapp/response/MovieSearchResponse.java
@@ -0,0 +1,33 @@
+package com.arfeenkhan.moviesapp.response;
+
+import com.arfeenkhan.moviesapp.models.MovieModel;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieSearchResponse {
+    @SerializedName("results")
+    @Expose()
+    private int total_count;
+
+    @SerializedName("results")
+    @Expose()
+    private List<MovieModel> movies;
+
+    public int getTotal_count(){
+        return total_count;
+    }
+
+    public List<MovieModel> getMovies(){
+        return movies;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSearchResponse{" +
+                "total_count=" + total_count +
+                ", movies=" + movies +
+                '}';
+    }
+}

f9936c0cfd0b2c978857d12592b23a4198272705
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-05 21:37:42 +0530

    initial commit

---
+++ app/src/main/java/com/arfeenkhan/moviesapp/utils/Credentials.java
@@ -0,0 +1,9 @@
+package com.arfeenkhan.moviesapp.utils;
+
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://developers.themoviedb.org";
+
+    public static final String API_KEY = "b7a93cef852e36bf998da52b666b71a8";
+}
